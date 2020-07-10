1ce26a2aaa4314c29771161bc116c546f3606e47
Adam-Young <awy6448@gmail.com>
2020-07-10 12:34:09 -0400

    finished last endpoint and created a react app templete.

--- routes/posts-router.js
+++ routes/posts-router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const db = require("../data/db.js")
 
 //Create Operations
-router.post('/', (req, res) => {
+router.post('/', (req, res) => { // posts
   let post = req.body;
   if (post.title && post.contents){
     db.insert(req.body)
@@ -18,7 +18,7 @@ router.post('/', (req, res) => {
   }
 })
 
-router.post('/:id/comments', (req, res) => {
+router.post('/:id/comments', (req, res) => { // comments
   const id = req.params.id;
   let comment = req.body;
   Object.assign(comment, {post_id: id})
@@ -42,7 +42,7 @@ router.post('/:id/comments', (req, res) => {
 })
 
 // Read Operations
-router.get("/", (req, res) => {
+router.get("/", (req, res) => { // ALL posts
   db.find(req.query)
   .then(posts => {
     res.status(200).json(posts)
@@ -55,7 +55,7 @@ router.get("/", (req, res) => {
   })
 })
 
-router.get("/:id", (req, res) => {
+router.get("/:id", (req, res) => { // posts by id
   db.findById(req.params.id)
   .then(post => {
     console.log(post)
@@ -71,7 +71,7 @@ router.get("/:id", (req, res) => {
   })
 })
 
-router.get('/:id/comments', (req, res) => {
+router.get('/:id/comments', (req, res) => { // comments by post id
   const {id} = req.params;
   try{
     db.findPostComments(id)
@@ -88,7 +88,7 @@ router.get('/:id/comments', (req, res) => {
 })
 
 // delete operations
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res) => { // delete post by id
   const {id} = req.params;
   let post
   db.findById(id)
@@ -102,7 +102,30 @@ router.delete('/:id', (req, res) => {
 
 })
 
+//Update operations
+router.put("/:id", (req, res) => {
+  const newPost = req.body;
+  const {id} = req.params;
+  
+  if (newPost.title && newPost.contents) {
+      db.update(id, newPost)
+      .then(success => {
+          if (success !== 1) {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+          }else{
+            res.status(200).json(newPost)
+          }
+        
+      })
+      .catch(err => res.status(500).json({ error: "The post information could not be modified." }))
+    
+  }else{
+    res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+  }
+    
+    
 
+})
 
 
 module.exports = router
